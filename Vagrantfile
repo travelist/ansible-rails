5458d4db5657e225a5863cb8052e814c183ed507
Komei Shimamura <komei.t.f@gmail.com>
2016-01-23 15:01:10 +0900

    Add rails configuration sample

--- Vagrantfile
+++ Vagrantfile
@@ -4,11 +4,11 @@
 Vagrant.configure(2) do |config|
   config.vm.box = "ubuntu/trusty64"
   config.vm.network "private_network", ip: "192.168.33.3"
-  # config.vm.provider "virtualbox" do |vb|
-  #   # Display the VirtualBox GUI when booting the machine
-  #   vb.gui = true
-  #
-  #   # Customize the amount of memory on the VM:
-  #   vb.memory = "1024"
-  # end
+  config.vm.provider "virtualbox" do |vb|
+    #   # Display the VirtualBox GUI when booting the machine
+    #   vb.gui = true
+    #
+    #   # Customize the amount of memory on the VM:
+    vb.memory = "2048"
+  end
 end
---
+++ rails_config/config/deploy.rb
@@ -0,0 +1,60 @@
+# config valid only for current version of Capistrano
+lock '3.4.0'
+
+# ---------------------------------------
+set :application, 'hello_world'
+set :repo_url, 'git@github.com:example.example.git'
+set :rbenv_ruby, '2.2.3'
+# ---------------------------------------
+
+# Unicorn
+set :unicorn_pid, "#{fetch(:deploy_to)}/shared/tmp/pids/unicorn.pid"
+set :unicorn_config_path, "#{fetch(:deploy_to)}/current/config/unicorn.rb"
+
+# Default branch is :master
+# ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
+
+# Default deploy_to directory is /var/www/my_app_name
+# set :deploy_to, '/var/www/my_app_name'
+
+# Default value for :scm is :git
+# set :scm, :git
+
+# Default value for :format is :pretty
+# set :format, :pretty
+
+# Default value for :log_level is :debug
+# set :log_level, :debug
+
+# Default value for :pty is false
+# set :pty, true
+
+# Default value for :linked_files is []
+# set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml')
+
+# Default value for linked_dirs is []
+# set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
+
+# Default value for default_env is {}
+# set :default_env, { path: "/opt/ruby/bin:$PATH" }
+
+# Default value for keep_releases is 5
+# set :keep_releases, 5
+
+# shared_path = "#{fetch(:deploy_to)}/shared"
+
+after 'deploy:publishing', 'deploy:restart'
+
+namespace :deploy do
+  # after :restart, :clear_cache do
+    # on roles(:web), in: :groups, limit: 3, wait: 10 do
+      # Here we can do anything such as:
+      # within release_path do
+      #   execute :rake, 'cache:clear'
+      # end
+    # end
+  # end
+  task :restart do
+    invoke 'unicorn:restart'
+  end
+end
